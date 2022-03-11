f1abb91be95b76df0e3758d8ac7b7d09b06d5ef3
LazizbekShodmonov <lazizbekshodmonov001@gmail.com>
2022-03-11 16:24:51 +0500

    update

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 let   display = document.querySelector('.display'),
       value = document.querySelectorAll('.value'),
       equal = document.querySelector('.equal'),
-      clear = document.querySelector('.clear'),
-      plus = document.querySelector('.plus'),
-      multiply = document.querySelector('.multiply'),
-      divide = document.querySelector('.divide'),
-      subtract = document.querySelector('.subtract'),
-      dot = document.querySelector('.dot'),
+      clear = documgitent.querySelector('.clear'),
       result;
 
 value.forEach((item, i) => {
@@ -15,13 +10,10 @@ value.forEach((item, i) => {
             display.innerHTML += `${number}`;
             
       });
-      
 });
-
 clear.addEventListener('click', (item)=>{
       display.innerHTML = '';     
 });
-
 equal.addEventListener('click', (item)=>{
      result = eval(display.textContent);
      display.innerHTML = `${result}`;
